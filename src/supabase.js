b4ffb8af7162ce644487507a17a5c0c561995cf7
Mustapha Olabode Sulaimon <90554525+weblord30@users.noreply.github.com>
2026-08-06 02:20:33 +0100

    Add ADMIN_EMAIL constant to supabase.js
    Added ADMIN_EMAIL constant for admin user.

--- src/supabase.js
+++ src/supabase.js
@@ -4,3 +4,4 @@ const SUPABASE_URL = 'https://bcmsmhxmyjnftfthgtka.supabase.co'
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJjbXNtaHhteWpuZnRmdGhndGthIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODU5NTk1NTUsImV4cCI6MjEwMTUzNTU1NX0.D0gWi134RR7u7rHGCovL03HOZqwgGgX6bbtd2TSmZHY'
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
+export const ADMIN_EMAIL = 'at.trials00@gmail.com'
